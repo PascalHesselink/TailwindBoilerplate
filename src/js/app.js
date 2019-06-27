af773cf8f6aeac244c81425eb75ca2fc5f128ae2
Pascal Hesselink <mail@pascalhesselink.nl>
2019-06-27 22:32:20 +0200

    Initial Commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            message: 'Tailwind Template',
+            versionMessage: 'Using Tailwind {version}',
+            version: '1.0.4',
+        };
+    },
+});
---
+++ tailwind.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            fontSize: {
+                'main-heading': '90px',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
